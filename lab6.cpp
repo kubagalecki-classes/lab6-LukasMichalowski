6ac264c87b3db3cf9cfcbf45e941cc4a6c1c5d20
LukasMichalowsk <lukpio97@gmail.com>
2020-12-11 11:03:04 +0000

    Test 1

--- lab6.cpp
+++ lab6.cpp
@@ -1,6 +1,28 @@
+
+#include "make_random_vector.hpp"
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <complex>
+#include <functional>
 #include <iostream>
+#include <iterator>
+#include <list>
+#include <numeric>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-    puts("Hello, World!");
+    vector< int > v = make_random_vector(10, 0, 10);
+
+    copy(v.begin(), v.end(), ostream_iterator< double >(cout, " "));
+    cout << endl;
+
+    auto it = partition(v.begin(), v.end(), [](int i) { return i > 6; });
+    sort(v.begin(), it);
+    copy(v.begin(), v.end(), ostream_iterator< double >(cout, " "));
+    cout << endl;
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -5,12 +5,21 @@
 #include <algorithm>
 #include <list>
 #include <vector>
-
+using namespace std;
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
+    auto                lambda1 = [&](Human n) { n.birthday(); };
+    std::for_each(people.begin(), people.end(), lambda1);
 
-    // Twoja implementacja tutaj
+    auto lambda2 = [](Human n) {
+        if (n.isMonster() == true)
+            return 'n';
+        else
+            return 'y';
+    };
+
+    std::transform(people.begin(), people.end(), ret_v.begin(), lambda2);
 
     return ret_v;
 }
